cfe641c96fddfd3b3d06e7df94d922eb6b57c6e8
DuoPan <pd54007@gmail.com>
2017-09-18 12:27:01 +1000

    first commit

---
+++ FIT5140A2V2/Queue.swift
@@ -0,0 +1,13 @@
+//
+//  Queue.swift
+//  FIT5140A2V2
+//
+//  Created by duo pan on 12/9/17.
+//  Copyright © 2017 duo pan. All rights reserved.
+//
+
+import UIKit
+
+class Queue: NSObject {
+
+}
---
+++ FIT5140A2V2/SensorOneController.swift
@@ -0,0 +1,35 @@
+//
+//  SensorOneController.swift
+//  FIT5140A2V2
+//
+//  Created by duo pan on 14/9/17.
+//  Copyright © 2017 duo pan. All rights reserved.
+//
+
+import UIKit
+
+class SensorOneController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
