d5911c8a65a8df0e06d32c9718db4ee7d7aed0cb
DuoPan <pd54007@gmail.com>
2017-09-22 11:24:34 +1000

    main page

--- FIT5140A2V2/ViewController.swift
+++ FIT5140A2V2/ViewController.swift
@@ -10,12 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    @IBOutlet var button4: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         button1.setImage(#imageLiteral(resourceName: "sensor1"), for: .normal)
         button1.contentMode = .scaleAspectFit
+        button2.setImage(#imageLiteral(resourceName: "sensor2"), for: .normal)
+        button2.contentMode = .scaleAspectFit
+        button3.setImage(#imageLiteral(resourceName: "color1"), for: .normal)
+        button3.contentMode = .scaleAspectFit
+        button4.setImage(#imageLiteral(resourceName: "color2"), for: .normal)
+        button4.contentMode = .scaleAspectFit
     }
 
     override func didReceiveMemoryWarning() {
